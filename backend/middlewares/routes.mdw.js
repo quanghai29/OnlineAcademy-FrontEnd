425871b3eb606f186a38ac51c6456b9ea7b46802
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-06-27 21:40:19 +0700

    add authentication

--- backend/middlewares/routes.mdw.js
+++ backend/middlewares/routes.mdw.js
@@ -1,6 +1,6 @@
-// const restrict = require('./auth.mdw');
 const swaggerUI = require('swagger-ui-express');
 const {openapiSpecification} = require('../utils/swagger');
+// const auth = require('./auth.mdw');
 
 module.exports = function (app) {
   // link default
@@ -17,6 +17,7 @@ module.exports = function (app) {
   ));
   
   //routers path here
+  app.use('/auth', require('../routes/auth.route'));
   app.use('/course', require('../routes/course.route'));
   app.use('/lecturer/course', require('../routes/lecturer/course.route'));
   app.use('/lecturer/chapter', require('../routes/lecturer/chapter.route'));
--- backend/routes/account.route.js
+++ backend/routes/account.route.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 const accountService = require('../services/account.service');
 
 
 const signupSchema = require('../schema/account/signup.account.json')
 router.post('/signup', require('../middlewares/validate.mdw')(signupSchema), async (req, res) => {
   const newAcc = req.body;
+  newAcc.password = bcrypt.hashSync(newAcc.password, 10);
   const ret = await accountService.createAcc(newAcc);
+  delete newAcc.password;
   res.status(ret.code).json(ret.data);
 })
 
