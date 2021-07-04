ca8a149eac5dc70242e37b404ac147c663aa27e8
LinhDong64 <mailinhdong@gmail.com>
2021-07-04 17:59:31 +0700

    replace asset folder

--- fontend/src/components/Account/HeaderForm/index.js
+++ fontend/src/components/Account/HeaderForm/index.js
@@ -1,10 +1,10 @@
-import headerForm from '../../../assets/images/account/header_form.png'
 
 export default function HeaderForm(props) {
 
+  const headerFormImg = 'assets/images/account/header_form.png';
   return (
     <div className={props.class}>
-      <img src={headerForm} alt=""></img>
+      <img src={headerFormImg} alt=""></img>
       <br></br>
       <span>{props.spanValue}</span>
     </div>
