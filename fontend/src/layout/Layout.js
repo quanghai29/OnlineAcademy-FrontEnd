b197088fe17eabe03ec6af1c11b4edb9f98dc506
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-07-04 22:24:13 +0700

    add layout view

--- fontend/src/layout/Layout.js
+++ fontend/src/layout/Layout.js
@@ -1,10 +1,12 @@
 import MainNavigation from './MainNavigation';
+import Footer from './Footer';
 
 function Layout({ children }) {
   return (
     <div>
       <MainNavigation />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 }
---
+++ fontend/src/layout/MainNavigation/index.js
@@ -0,0 +1,46 @@
+// import { Link } from 'react-router-dom';
+import classes from './MainNavigation.module.scss';
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <img
+          src='assets/images/header/Logo.png'
+          alt="logo header"
+        />
+      </div>
+      <div className={classes.menu}>
+        <img
+          className={classes.menuImg}
+          src='assets/images/header/menu.png'
+          alt="menu"
+        />
+        <p className={classes.menuTitle}>Danh mục</p>
+      </div>
+      <div className={classes.search}>
+        <img
+          src='assets/images/header/search.png'
+          alt="search"
+        />
+        <input type="text" placeholder="Tìm kiếm khóa học"/>
+      </div>
+      <div className={classes.login}>
+          <button>Đăng nhập</button>
+          <button>Đăng ký</button>
+      </div>
+      {/* <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/another'>Another</Link>
+          </li>
+        </ul>
+      </nav> */}
+    </header>
+  );
+}
+
+export default MainNavigation;
