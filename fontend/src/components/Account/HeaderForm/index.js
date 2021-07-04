c7e43978c5b7b7a383d98ce946eeef0c1c1da45c
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-07-04 22:40:39 +0700

    pull to commit

--- fontend/src/components/Account/HeaderForm/index.js
+++ fontend/src/components/Account/HeaderForm/index.js
@@ -1,10 +1,10 @@
-import headerForm from '../../../assets/images/account/header_form.png'
+// import headerForm from '../../../assets/images/account/header_form.png'
 
 export default function HeaderForm(props) {
 
   return (
     <div className={props.class}>
-      <img src={headerForm} alt=""></img>
+      <img src="assets/images/account/header_form.png" alt=""></img>
       <br></br>
       <span>{props.spanValue}</span>
     </div>
