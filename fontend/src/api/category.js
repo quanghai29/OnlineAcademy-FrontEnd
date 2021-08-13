f5ed80b0346748e7e8f771e0e5f945aeb3c23f44
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-08-13 17:43:17 +0700

    add some logic handlers

--- fontend/src/api/category.js
+++ fontend/src/api/category.js
@@ -7,5 +7,13 @@ export const category = {
     const respone = await appAPI.get(`/category`,{ validateStatus: false })
     return respone;
   },
+  getHotCategories: async () => {
+    try {
+      const { data } = await appAPI.get('/course/category-mostbestseller');
+      return data;
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
 }
 // #endregion
--- fontend/src/redux/sagas/getHotCourses.js
+++ fontend/src/redux/sagas/getHotCourses.js
@@ -1,20 +1,19 @@
 import { takeEvery, call, put } from 'redux-saga/effects';
-import axios from 'axios';
+import { getCourses } from '../../api/course';
 import * as type from '../constants/actionTypes';
 import { setHotCourse, fetchHotCourseFail } from '../actions/hotCourses';
 
 function* fetchHotCourse() {
     try {
-        const response = yield call(axios.get, 'http://localhost:5050/hotCourses');
-        console.log(response.data);
-        yield put(setHotCourse(response.data));
+        const data = yield call(getCourses.getHotCourses);
+        yield put(setHotCourse(data));
     } catch (error) {
         yield put(fetchHotCourseFail(error.message));
     }
 }
 
 function* watchFetchHotCourse() {
-    yield takeEvery(type.FETCH_COURSES, fetchHotCourse);
+    yield takeEvery(type.FETCH_HOT_COURSES, fetchHotCourse);
 }
 
 export default watchFetchHotCourse;
