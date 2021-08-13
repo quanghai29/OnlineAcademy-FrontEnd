09d50709a1e3870e8f94fbabb1c6441b5718d94e
LinhDong64 <mailinhdong@gmail.com>
2021-08-13 20:52:29 +0700

    handle profile(view admin)

--- fontend/src/components/User/Profile/ProfileContainer.js
+++ fontend/src/components/User/Profile/ProfileContainer.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import * as userApi from "../../../api/user";
-import { DOMAIN_API} from "../../../redux/constants/common"
+import { DOMAIN_API } from "../../../redux/constants/common"
 import { Link } from "react-router-dom";
 import styles from "./Profile.module.scss"
 
 
 const ProfileContainer = ({ userId }) => {
   const [userInfo, setUserInfo] = useState({});
- 
+
   useEffect(() => {
-    async function getUserProfile(id){
+    async function getUserProfile(id) {
       const data = await userApi.getProfile.getDetailInfoUser(id);
       setUserInfo(data);
     }
@@ -32,17 +32,34 @@ const ProfileContainer = ({ userId }) => {
           <div className="card-content">
             <span className="card-title activator grey-text text-darken-4">Hiển thị thông tin
               <i className="material-icons right">more_vert</i></span>
-              <p><Link to='/'>Quay lại trang chủ</Link></p>
+            <p><Link to='/'>Quay lại trang chủ</Link></p>
           </div>
           <div className="card-reveal">
-            <span className="card-title grey-text text-darken-4">Thông tin
-              <i className="material-icons right">close</i></span>
-            <div>  <span>Họ tên: {userInfo.fullname || 'Không biết'}</span></div>
-            <div><span>{userInfo.headline || ''}</span></div>
-            <div><span>Email: {userInfo.email || 'Không biết'}</span></div>
-            <div>
-              <span>Mô tả ngắn</span>
-              <p>{userInfo.description || ''}</p>
+            <div className={styles['user-info-container']}>
+              <div>
+                <span className="card-title grey-text text-darken-4">Thông tin
+                  <i className="material-icons right">close</i>
+                </span>
+              </div>
+              <div>
+                <span className={styles['title']}>Họ tên: </span>
+                <span>{userInfo.fullname || 'Không biết'}</span>
+              </div>
+              <div>
+                <span className={styles['title']}>Về bản thân: </span>
+                <span>{userInfo.headline || ''}</span>
+              </div>
+              <div>
+                <span className={styles['title']}>Email: </span>
+                <span>{userInfo.email || 'Không biết'}</span>
+              </div>
+              <div style={{ marginTop: '20px' }}>
+                <span className={styles['title']}>
+                  Mô tả ngắn
+                </span>
+                <p className={styles['description']}>{userInfo.description || ''}
+                </p>
+              </div>
             </div>
           </div>
         </div>
