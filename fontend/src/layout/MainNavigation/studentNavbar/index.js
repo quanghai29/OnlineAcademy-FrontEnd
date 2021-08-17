3a6691284e40de09699db3fa4dd5a6dbb6ad76ce
quanghai29 <lequanghai540@gmail.com>
2021-08-17 21:15:11 +0700

    fix click

--- fontend/src/layout/MainNavigation/studentNavbar/index.js
+++ fontend/src/layout/MainNavigation/studentNavbar/index.js
@@ -4,13 +4,19 @@ import Avartar from '../../../components/Common/Avartar';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import { useDispatch } from 'react-redux';
 import * as actionType from '../../../redux/constants/actionTypes'
+import { useEffect } from 'react';
 
 export default function StudentNavbar(props) {
-  const elems = document.querySelectorAll('.dropdown-trigger');
-  // eslint-disable-next-line
-  const instances = M.Dropdown.init(elems, {});
+ 
   const history = useHistory();
   const dispatch = useDispatch();
+
+  useEffect(function(){
+    const elems = document.querySelectorAll('.dropdown-trigger');
+    // eslint-disable-next-line
+    const instances = M.Dropdown.init(elems, {});
+  },[props])
+
   const logout = ()=>{
     dispatch({
       type: actionType.FETCH_LOGOUT
@@ -24,7 +30,7 @@ export default function StudentNavbar(props) {
       <li style={{ position: "relative" }}>
 
         {/* eslint-disable-next-line */}
-        <a className={`dropdown-trigger btn ${classes.dropdown}`} href="#" data-target="dropdown1">
+        <a className={`dropdown-trigger btn ${classes.dropdown}`} data-target="dropdown1">
           <Avartar {...{ nickName: props.fullname }} />
         </a>
 
